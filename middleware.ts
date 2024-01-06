5461bbe9f6d961ee77fb295738176d348be61e02
Nando <nandolopez009@gmail.com>
2024-01-05 22:50:14 -0600

    uninstalled clerk

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,1 @@
-import { authMiddleware } from "@clerk/nextjs";
-import  prisma  from "./lib/prisma"
 
-export default authMiddleware({
-  publicRoutes: ["/", "/contact"],
-});
- 
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
- 
